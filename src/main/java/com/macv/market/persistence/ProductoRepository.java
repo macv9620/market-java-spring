8c8b1a4b7c7bf271dffcb0947703e4bbefc67c8a
Mateo Vasco <macv9620@utp.edu.co>
2023-09-13 23:25:11 -0500

    Se finaliza la implementación de Data Mapper

--- src/main/java/com/macv/market/persistence/ProductoRepository.java
+++ src/main/java/com/macv/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.macv.market.domain.repository.ProductRepository;
 import com.macv.market.persistence.crud.ProductoCrudRepository;
 import com.macv.market.persistence.entity.Producto;
 import com.macv.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,10 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
@@ -33,33 +37,20 @@ public class ProductoRepository implements ProductRepository {
         return mapper.toProducts(productos);
     }
 
-    //En este caso ProductoCrudRepository por defecto ya implementa findById
-    public Optional<Producto> getProductoById(int idProducto){
-        return productoCrudRepository.findById(idProducto);
-    }
     @Override
     public Optional<Product> getById(int productId) {
         Optional<Producto> producto = productoCrudRepository.findById(productId);
         return producto.map(p -> mapper.toProduct(p));
     }
 
-    //En este caso ProductoCrudRepository por defecto ya implementa save
-    public Producto saveProducto(Producto producto){
-        return productoCrudRepository.save(producto);
-    }
-
     @Override
     public Product save(Product product) {
-        return null;
-    }
-
-    //En este caso ProductoCrudRepository por defecto ya implementa deleteById
-    public void deleteProducto(int idProducto){
-        productoCrudRepository.deleteById(idProducto);
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
     @Override
     public void delete(int productId) {
-
+        productoCrudRepository.deleteById(productId);
     }
 }
